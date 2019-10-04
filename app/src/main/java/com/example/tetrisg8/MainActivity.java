17c44e2397aa16ac9955fc09ee3bccae26960f62
ArshiaSaleem98 <45390300+ArshiaSaleem98@users.noreply.github.com>
2019-10-04 21:05:07 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/tetrisg8/MainActivity.java
+++ app/src/main/java/com/example/tetrisg8/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.tetrisg8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GameView gameView;
+    private FichaView fichaView;
+    private TextView caja_score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        this.caja_score = (TextView)findViewById(R.id.caja_score);
+        //to set score
+        //this.caja_score.setText(String.valueOf());
+        this.gameView = new GameView(this);
+        this.fichaView = new FichaView(this);
+
+        LinearLayout gameLayout = (LinearLayout)findViewById(R.id.gameView);
+        LinearLayout fichaLayout = (LinearLayout)findViewById(R.id.fichaView);
+
+        gameLayout.addView(this.gameView);
+        fichaLayout.addView(this.fichaView);
+     
     }
     
     
@@ -20,5 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void puntuacionFinal(View view) {
 
+    }
+    //move left
+    public void moveLeft(View view) {
+    }
+
+    //move right
+    public void moveRight(View view) {
+    }
+
+    //rotate
+    public void rotateAction(View view) {
     }
 }
