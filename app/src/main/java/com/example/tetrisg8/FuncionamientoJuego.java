05da283454a74b2eb52f2a3d592a346ab32d63fb
luuromero97 <43638829+luuromero97@users.noreply.github.com>
2019-10-05 19:38:49 +0200

    Añadido comprobar líneas y actualizar puntuación

--- app/src/main/java/com/example/tetrisg8/FuncionamientoJuego.java
+++ app/src/main/java/com/example/tetrisg8/FuncionamientoJuego.java
@@ -7,4 +7,26 @@ public class FuncionamientoJuego {
         Pieza pieza = new Pieza (tipoPieza);
         return pieza;
     }
+
+    public int lineasCompletas (Tablero tablero, Pieza piezaActual){ //comprobamos todas las filas en las que se encuentra la pieza actual una vez haya acabado de caer
+        int nLineasCompletas = 0;
+        for (int i = 0; i<4; i++){
+            boolean comprobarCompleta = true;
+            int filaAComprobar = piezaActual.getPieza()[i].getX();
+            for (int j = 0; j<10; j++){
+                if (tablero.getTablero()[filaAComprobar][j].getTipoPieza() == 0){
+                    comprobarCompleta = false;
+                }
+            }
+            if (comprobarCompleta){
+                nLineasCompletas++;
+            }
+        }
+        return nLineasCompletas;
+    }
+
+    public int actualizarPuntuacion(int puntuacion, Tablero tablero, Pieza pieza){ //le pasamos la puntuación actual
+        puntuacion = puntuacion + lineasCompletas(tablero, pieza)*30;
+        return puntuacion;
+    }
 }
